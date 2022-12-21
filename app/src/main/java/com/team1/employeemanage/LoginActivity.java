035b3adcf1354ce9fc5fb04096f86b8c617a4b0e
Dương Hoàng Tiến <htduong25@gmail.com>
2022-12-21 22:39:51 +0700

    Update Dash Board activity_dash_board.xml

--- app/src/main/java/com/team1/employeemanage/LoginActivity.java
+++ app/src/main/java/com/team1/employeemanage/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.team1.employeemanage;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private EditText username, password;
     private Button btn_SignIn;
 
     @Override
@@ -16,25 +30,66 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        mAuth = FirebaseAuth.getInstance();
+
         getSupportActionBar().hide();
 
         addConttrols();
 
         addEvent();
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if(currentUser != null){
+            UpdateUI(currentUser);
+        }
     }
 
     private void addEvent() {
         btn_SignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, DashBoardActivity.class);
-                startActivity(intent);
-                finish();
+
+                SignInWithEmailAndPassword();
             }
         });
     }
 
+    private void SignInWithEmailAndPassword() {
+        String username_ = username.getText().toString();
+        String password_ = password.getText().toString();
+
+        mAuth.signInWithEmailAndPassword(username_, password_)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            UpdateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Username or Password are incorrect",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+    }
+
+    private void UpdateUI(FirebaseUser user) {
+        Intent intent = new Intent(LoginActivity.this, DashBoardActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
     private void addConttrols() {
-        btn_SignIn = this.<Button>findViewById(R.id.button_SignIn);
+        btn_SignIn = this.<Button>findViewById(R.id.button_SignIn_Login);
+        username = this.<EditText>findViewById(R.id.editText_UserName_Login);
+        password = this.<EditText>findViewById(R.id.editText_Password_Login);
     }
 }
--- app/src/main/java/com/team1/employeemanage/SettingActivity.java
+++ app/src/main/java/com/team1/employeemanage/SettingActivity.java
@@ -2,9 +2,16 @@ package com.team1.employeemanage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class SettingActivity extends AppCompatActivity {
+
+    private Button button_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +19,25 @@ public class SettingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_setting);
 
         getSupportActionBar().hide();
+
+        addControls();
+
+        addEvents();
+    }
+
+    private void addEvents() {
+        button_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(SettingActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+
+    private void addControls() {
+        button_logout = this.<Button>findViewById(R.id.buttonLogout_Setting);
     }
 }
